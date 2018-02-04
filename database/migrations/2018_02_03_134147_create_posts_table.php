b6029889824660cdba5f71fb163d17fa1145af3b
drdlee <danielrudi.lee@gmail.com>
2018-02-04 22:16:54 +0800

    adding SoftDeletes

--- database/migrations/2018_02_03_134147_create_posts_table.php
+++ database/migrations/2018_02_03_134147_create_posts_table.php
@@ -18,6 +18,7 @@ class CreatePostsTable extends Migration
             $table->integer('user_id');
             $table->string('title');
             $table->text('content');
+            $table->softDeletes();
             $table->timestamps();
         });
     }
